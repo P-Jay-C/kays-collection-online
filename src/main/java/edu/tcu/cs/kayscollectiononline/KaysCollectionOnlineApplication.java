26daea6f5e1916f9bdb98d7cb9d213ce0a7b7570
Jeremiah Azilaku <jeremiah.azilaku@amalitech.com>
2023-12-14 07:56:39 +0000

    This is the first commit

---
+++ src/main/java/edu/tcu/cs/kayscollectiononline/KaysCollectionOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.kayscollectiononline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaysCollectionOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaysCollectionOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/tcu/cs/kayscollectiononline/KaysCollectionOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.kayscollectiononline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaysCollectionOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
